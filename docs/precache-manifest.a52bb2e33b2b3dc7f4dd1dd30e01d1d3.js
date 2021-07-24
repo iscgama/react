151643597fbe8f709ba9cc4fc0b21b38cbca508c
Marco Alberto <iscgama@gmail.com>
2021-07-24 12:03:56 -0500

    docs agregado

---
+++ docs/precache-manifest.a52bb2e33b2b3dc7f4dd1dd30e01d1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a0b406ea5a7fa9a9256abb28cbaff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60711a02f5561575e593",
+    "url": "/static/css/main.e57a3438.chunk.css"
+  },
+  {
+    "revision": "10a88db9f0d50ee9ff0c",
+    "url": "/static/js/2.b1d89741.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.b1d89741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60711a02f5561575e593",
+    "url": "/static/js/main.c3811392.chunk.js"
+  },
+  {
+    "revision": "069e29a66ffcdb7feb5c",
+    "url": "/static/js/runtime-main.2d662563.js"
+  }
+]);
